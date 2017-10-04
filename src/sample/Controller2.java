5ad58a1c1b2c17239987a11fb049c860b5d63820
qi5 <qi5@kth.se>
2017-10-04 12:28:12 +0200

    switch and defaut value set

--- src/sample/Controller2.java
+++ src/sample/Controller2.java
@@ -12,6 +12,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
@@ -46,6 +47,22 @@ public class Controller2 implements Initializable {
     @FXML
     public BorderPane background;
 
+    @FXML
+    public ToggleButton infectSW;
+    @FXML
+    public ToggleButton deadSW;
+    @FXML
+    public ToggleButton recoverySW;
+    @FXML
+    public ToggleButton illSW;
+    @FXML
+    public ToggleButton tInfectSW;
+    @FXML
+    public ToggleButton tDeathSW;
+
+
+
+
     private int days;
     private int deathCounter;
     private int infectedCounter;
@@ -69,7 +86,9 @@ public class Controller2 implements Initializable {
     int[][] mouseRec;
     @FXML
     public void start(ActionEvent actionEvent) {
+        //initialization for start button
         int populationInt= Integer.parseInt(population.getText());
+        outputBox.setText("Please click on the empty space on the right to choose where has the initial patient been deteced. ");
         if (populationInt < 74 * 80) {
             maxX = (int) Math.floor(Math.sqrt(populationInt));
             maxY = populationInt / maxX;
@@ -85,7 +104,7 @@ public class Controller2 implements Initializable {
         }
         int initInt= Integer.parseInt(init.getText());
         mouseRec= new int[initInt][2];
-
+        //initialize people get sick and where are they
         final int[] xAxis = new int[1];
         final int[] yAxis = new int[1];
         int[][] record=new int[initInt][2];
@@ -100,17 +119,21 @@ public class Controller2 implements Initializable {
             original=copy;
             counter++;
         });
+
+        //start simulation
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
+                //if total number of initilized people are successfully been input and accepted
                 if (counter>=initInt) {
                     display.setOnMouseClicked(null);
-                    //probability go though neighbor cell
+                    //start a new day per second
                     if (now - lastUpdate >= 1000_000_000) {
                         resetAllDCounter();
                         for (int y = 0; y < maxY; y++) {
                         for (int x = 0; x < maxX; x++) {
                             if (original[x][y] == true) {
+
                                 //death
                                 Random dRand = new Random();
                                 double dTest = dRand.nextDouble();
@@ -134,6 +157,7 @@ public class Controller2 implements Initializable {
                                     //gc.clearRect(x* 10,y* 10,10,10);
                                     break;
                                 }
+                                //probability go though neighbor cell(8 of them)
                                 if (x > 0 + 1 && y > 0 + 1 && x < maxX - 1 && y < maxY - 1) {
                                     //8 neighbors
                                     boolean[][] copy = original;//copy
@@ -160,7 +184,27 @@ public class Controller2 implements Initializable {
                         illCounter=illCounter+infectedCounter-deathCounter-recoverCounter;
                         accumInfecC=accumInfecC+infectedCounter;
                         accumDeathcC=accumDeathcC+deathCounter;
-                        outputBox.setText("Day "+ days +" - \n "+ deathCounter+" people has dead; \n"+infectedCounter+" people has been infected today; \n"+ recoverCounter+" people has been recovered today; \n"+illCounter+" people are still ill today; \n" +accumInfecC+" total amount of people has infected until today \n"+accumDeathcC+" total amount of people has dead until today \n");
+                        outputBox.setText("Day "+ days+" - \n ");
+                        if(infectSW.isSelected()){
+                            outputBox.setText(outputBox.getText()+infectedCounter+" people has been infected today; \n");
+                        }
+                        if(deadSW.isSelected()){
+                            outputBox.setText(outputBox.getText()+deathCounter+" people has dead; \n");
+                        }
+                        if(recoverySW.isSelected()){
+                            outputBox.setText(outputBox.getText()+recoverCounter+" people has been recovered today; \n");
+                        }
+                        if(illSW.isSelected()){
+                            outputBox.setText(outputBox.getText()+illCounter+" people are still ill today; \n" );
+
+                        }
+                        if(tInfectSW.isSelected()){
+                            outputBox.setText(outputBox.getText()+accumInfecC+" total amount of people has infected until today \n" );
+                        }
+                        if(tDeathSW.isSelected()){
+                            outputBox.setText(outputBox.getText()+accumDeathcC+" total amount of people has dead until today \n");
+
+                        }
                         days++;
                 }
                     }
@@ -172,8 +216,25 @@ public class Controller2 implements Initializable {
             timer.start();
 
     }
+    public void death(){
+        //death
+        Random dRand = new Random();
+        double dTest = dRand.nextDouble();
+        if (dTest < Double.parseDouble(death.getText())) {
+            originalT[x][y] = -2;
+            original[x][y] = false;
+            deathCounter++;
+            gc.setFill(Color.YELLOW);
+            gc.fillRect(x * 10, y * 10, 10, 10);
+            break;
+        }
+    }
+
+    //before pressing the start button
+    //initial state
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //geneneral initialization for overall application
         days=0;
         deathCounter=0;
         infectedCounter=0;
@@ -182,7 +243,24 @@ public class Controller2 implements Initializable {
         accumInfecC=0;
         accumDeathcC=0;
         lastUpdate = 0;
+        infectSW.setSelected(true);
+        deadSW.setSelected(true);
+        recoverySW.setSelected(true);
+        illSW.setSelected(true);
+        tInfectSW.setSelected(true);
+        tDeathSW.setSelected(true);
+
+        //default value
+        population.setText("5000");
+        infection.setText("0.75");
+        Min.setText("2");
+        Max.setText("7");
+        death.setText("0.3");
+        init.setText("1");
+        //user mistake elimination
+        outputBox.setText("Please input the initial value for each box. \n"+"For probability please use decimal number instead of percentage.\nFor example 0.75 instead of 75 percent. ");
     }
+    //reset the counters for yesterday
     public void resetAllDCounter(){
         deathCounter=0;
         infectedCounter=0;
@@ -190,6 +268,7 @@ public class Controller2 implements Initializable {
         illCounter=0;
     }
 
+    //gui update per change
     public void updateCanvas(int x,int y,boolean[][]copy){
             final int xReal = x;
             final int yReal = y;
